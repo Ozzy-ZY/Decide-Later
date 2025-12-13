b7284e73d1325ddbc91f009583f7adccdeecb14e
Ozzy-ZY <zyadhimself1@gmail.com>
2025-12-13 15:54:18 +0200

    Add FluentValidation-based validators for authentication DTOs
    - Created validators for `RegisterRequestDto`, `LoginRequestDto`, and `ChangePasswordRequestDto` to enforce input validation rules.

---
+++ Application/Validators/AuthValidators.cs
@@ -0,0 +1,75 @@
+using FluentValidation;
+using Application.DTOs.Auth;
+
+namespace Application.Validators;
+
+public class RegisterRequestDtoValidator : AbstractValidator<RegisterRequestDto>
+{
+    public RegisterRequestDtoValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(50).WithMessage("First name must not exceed 50 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(50).WithMessage("Last name must not exceed 50 characters");
+
+        RuleFor(x => x.UserName)
+            .NotEmpty().WithMessage("Username is required")
+            .MinimumLength(3).WithMessage("Username must be at least 3 characters")
+            .MaximumLength(30).WithMessage("Username must not exceed 30 characters")
+            .Matches(@"^[a-zA-Z0-9_]+$").WithMessage("Username can only contain letters, numbers, and underscores");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+            .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+            .Matches(@"[\!\?\*\.\@\#\$\%\^\&\+\=]").WithMessage("Password must contain at least one special character (!?*.@#$%^&+=)");
+
+        RuleFor(x => x.ConfirmPassword)
+            .NotEmpty().WithMessage("Confirm password is required")
+            .Equal(x => x.Password).WithMessage("Passwords do not match");
+    }
+}
+
+public class LoginRequestDtoValidator : AbstractValidator<LoginRequestDto>
+{
+    public LoginRequestDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
+
+public class ChangePasswordRequestDtoValidator : AbstractValidator<ChangePasswordRequestDto>
+{
+    public ChangePasswordRequestDtoValidator()
+    {
+        RuleFor(x => x.CurrentPassword)
+            .NotEmpty().WithMessage("Current password is required");
+
+        RuleFor(x => x.NewPassword)
+            .NotEmpty().WithMessage("New password is required")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+            .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+            .Matches(@"[\!\?\*\.\@\#\$\%\^\&\+\=]").WithMessage("Password must contain at least one special character (!?*.@#$%^&+=)")
+            .NotEqual(x => x.CurrentPassword).WithMessage("New password must be different from current password");
+
+        RuleFor(x => x.ConfirmNewPassword)
+            .NotEmpty().WithMessage("Confirm new password is required")
+            .Equal(x => x.NewPassword).WithMessage("Passwords do not match");
+    }
+}
